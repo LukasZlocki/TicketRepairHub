c270003b5a783b2d0cbb694d0505dfe5147c9982
lzlocki <lzlocki@poczta.fm>
2024-04-24 19:14:13 +0200

    add db context

---
+++ TicketRepairHub.Infrastructure/Persistance/RepairTicketDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketRepairHub.Domain.Models;
+
+namespace TicketRepairHub.Infrastructure.Persistance
+{
+    public class RepairTicketDbContext : DbContext
+    {
+        public RepairTicketDbContext(DbContextOptions<RepairTicketDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<FailureTreeTest> FailureTreeTests { get; set; }
+        public DbSet<FailureTreePart> FailureTreeParts { get; set; }
+        public DbSet<FailureTreePartFailure> FailureTreePartFailures { get; set; }
+        public DbSet<TestLimit> TestLimits { get; set; }
+        public DbSet<TestResult> TestResults { get; set; }
+        public DbSet <Ticket> Tickets { get; set; }
+        public DbSet <User> Users { get; set; }
+    }
+}
