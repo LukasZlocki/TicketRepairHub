9fa2b84c15e159a2beb26fe15134148b45e77e19
lzlocki <lzlocki@poczta.fm>
2024-04-24 19:27:34 +0200

    fix non nullables

--- TicketRepairHub.Domain/Models/FailureTreeTest.cs
+++ TicketRepairHub.Domain/Models/FailureTreeTest.cs
@@ -3,6 +3,6 @@
     public class FailureTreeTest
     {
         public int Id { get; set; }
-        public string TestDescription { get; set; }
+        public string? TestDescription { get; set; }
     }
 }
