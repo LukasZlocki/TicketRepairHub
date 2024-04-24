d386694855d6390e90100b3f42e66704a946f2b0
lzlocki <lzlocki@poczta.fm>
2024-04-24 19:53:57 +0200

    add infrastructure extension to connect to db

---
+++ TicketRepairHub.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketRepairHub.Infrastructure.Persistance;
+
+namespace TicketRepairHub.Infrastructure.Extensions
+{
+    public class ServiceCollectionExtensions
+    {
+        public static class ServiceCollectionExtension
+        {
+            public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+            {
+                services.AddDbContext<RepairTicketDbContext>(options => options.UseSqlServer(
+                    configuration.GetConnectionString("ShopConnectionString")));
+            }
+        }
+    }
+}
