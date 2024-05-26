7949b0b0b4cbce37d09b3363c2bf0d4f4a5d2e9f
lzlocki <lzlocki@poczta.fm>
2024-05-26 15:29:05 +0200

    Add seeding example

---
+++ TicketRepairHub.Infrastructure/Seeders/FailureTreeSeeder.cs
@@ -0,0 +1,46 @@
+﻿using TicketRepairHub.Infrastructure.Persistance;
+
+namespace TicketRepairHub.Infrastructure.Seeders
+{
+    public class FailureTreeSeeder
+    {
+        private readonly RepairTicketDbContext _dbcontext;
+
+        public FailureTreeSeeder(RepairTicketDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+
+        public async Task Seed()
+        {
+            if(await _dbcontext.Database.CanConnectAsync())
+            {
+                if (!_dbcontext.FailureTreeTests.Any())
+                {
+                    _dbcontext.FailureTreeTests.AddRange(
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test1"},
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test2" },
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test3" },
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test4" },
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test5" }
+                        );
+                    await _dbcontext.SaveChangesAsync();
+                }
+
+                /*
+                if (!_dbcontext.FailureTreeParts.Any())
+                {
+                    _dbcontext.FailureTreeTests.AddRange(
+                        new Domain.Models.FailureTreePart() { },
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test2" },
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test3" },
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test4" },
+                        new Domain.Models.FailureTreeTest() { TestDescription = "Test5" }
+                        );
+                    await _dbcontext.SaveChangesAsync();
+                }
+                */
+            }
+        }
+    }
+}
